57824d93b38814207c1ac6fd2e05e290031d7e56
Akash M <122703615+akashmangalore@users.noreply.github.com>
2024-08-28 11:06:32 +0000

    added p10k file

--- zsh/.zshrc
+++ zsh/.zshrc
@@ -139,3 +139,6 @@ fi
 
 # FZF
 [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
+
+# To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
+[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
--- zsh/install.sh
+++ zsh/install.sh
@@ -3,4 +3,5 @@
 set -e
 
 ln -sf "${DOTFILES_LOCATION}/zsh/.zshrc" "${HOME}/.zshrc"
+ln -sf "${DOTFILES_LOCATION}/zsh/.p10k.zsh" "${HOME}/.p10k.zsh"
 ln -sf "${DOTFILES_LOCATION}/zsh/benmatselby.zsh-theme" "${HOME}/.oh-my-zsh/custom/themes/"
